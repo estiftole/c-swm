bf94b222769c953474b7c67e97803eb806610e32
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-08-03 14:26:42 +0300

    fixed transition model bugs

--- modules.py
+++ modules.py
@@ -75,10 +75,10 @@ class EdgeModel(nn.Module):
         return h
 
 class NodeModel(nn.Module):
-    def __init__(self, input_dim: int, hidden_dim: int, act_fn: str = 'relu'):
+    def __init__(self, node_input_dim: int, input_dim: int, hidden_dim: int, act_fn: str = 'relu'):
         super().__init__()
 
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc1 = nn.Linear(node_input_dim, hidden_dim)
         self.act1 = utils.get_act_fn(act_fn)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.ln = nn.LayerNorm(hidden_dim)
@@ -90,10 +90,11 @@ class NodeModel(nn.Module):
             row, _ = edge_index
             # Aggregate messages passed to each node
             agg = edge_feat.new_zeros(node_feat.size(0), edge_feat.size(1)).index_add_(0, row, edge_feat)
-            h = torch.cat([node_feat, agg], dim=1)
         else:
-            h = node_feat
+            edge_dim = self.fc1.in_features - node_feat.size(1)
+            agg = node_feat.new_zeros(node_feat.size(0), edge_dim)
 
+        h = torch.cat([node_feat, agg], dim=1)
         h = self.act1(self.fc1(h))
         h = self.act2(self.ln(self.fc2(h)))
         h = self.fc3(h)
@@ -103,8 +104,10 @@ class NodeModel(nn.Module):
 class TransitionModel(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int, action_dim: int, num_slots: int, act_fn: str = 'relu') -> None:
         super().__init__()
+        self.input_dim = input_dim
         self.action_dim = action_dim
-        self._node_fn = NodeModel(input_dim, hidden_dim, act_fn)
+        node_input_dim = hidden_dim + input_dim + self.action_dim
+        self._node_fn = NodeModel(node_input_dim, input_dim, hidden_dim, act_fn)
         self._edge_fn = EdgeModel(input_dim, hidden_dim, act_fn)
 
         self.edge_list = None
@@ -114,7 +117,7 @@ class TransitionModel(nn.Module):
         if self.edge_list is None or self.batch_size != batch_size:
             self.batch_size = batch_size
 
-            nodes = torch.arange(num_slots)
+            nodes = torch.arange(num_slots, device=device)
             senders, receivers = torch.meshgrid(nodes, nodes, indexing="ij")
             mask = senders != receivers
             base_senders = senders[mask]
@@ -132,7 +135,7 @@ class TransitionModel(nn.Module):
         return self.edge_list
 
     def forward(self, states, action) -> torch.Tensor:
-        batch_size, num_nodes, _ = states.size
+        batch_size, num_nodes, _ = states.shape
 
         node_feat = states.view(-1, self.input_dim)
 
@@ -141,7 +144,7 @@ class TransitionModel(nn.Module):
 
         if num_nodes > 1:
             edge_index = self._get_edge_list_fully_connected(
-                batch_size, num_nodes, states.is_cuda)
+                batch_size, num_nodes, states.device)
 
             row, col = edge_index
             edge_feat = self._edge_fn( node_feat[row], node_feat[col], edge_feat)
