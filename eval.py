4b9059eb61f28a5b12a38859e8d2e9d431d9cca8
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-08-07 20:06:40 +0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,6 +1,5 @@
 import argparse
 import torch
-import torch.nn.functional as F
 import utils
 import os
 import pickle
@@ -29,7 +28,6 @@ args_eval = parser.parse_args()
 
 meta_file = os.path.join(args_eval.save_folder, 'metadata.pkl')
 model_file = os.path.join(args_eval.save_folder, 'model.pt')
-decoder_file = os.path.join(args_eval.save_folder, 'decoder.pt')
 
 args = pickle.load(open(meta_file, 'rb'))['args']
 
@@ -48,9 +46,10 @@ device = torch.device('cuda' if args.cuda else 'cpu')
 dataset = utils.PathDataset(
     hdf5_file=args.dataset, path_length=args_eval.num_steps)
 eval_loader = data.DataLoader(
-    dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-obs = next(iter(eval_loader))[0]
+# Get data sample
+obs = eval_loader.__iter__().next()[0]
 input_shape = obs[0][0].size()
 
 model = models.ContrastiveSWM(
@@ -58,45 +57,33 @@ model = models.ContrastiveSWM(
     hidden_dim=args.hidden_dim,
     action_dim=args.action_dim,
     input_dims=input_shape,
-    num_slots=args.num_objects,
+    num_objects=args.num_objects,
     sigma=args.sigma,
     hinge=args.hinge,
-    global_action=args.global_action,
+    global_action=args.copy_action,
+    # encoder=args.encoder
 ).to(device)
+
+print(f'Running eval on {args_eval.num_steps} steps...')
+
 
 model.load_state_dict(torch.load(model_file))
 model.eval()
 
-decoder = None
-if getattr(args, 'decoder', False):
-    if args.encoder == 'large':
-        decoder = models.DecoderCNNLarge(input_dim=args.embedding_dim, num_objects=args.num_objects, hidden_dim=args.hidden_dim // 16, output_size=input_shape).to(device)
-    elif args.encoder == 'medium':
-        decoder = models.DecoderCNNMedium(input_dim=args.embedding_dim, num_objects=args.num_objects, hidden_dim=args.hidden_dim // 16, output_size=input_shape).to(device)
-    elif args.encoder == 'small':
-        decoder = models.DecoderCNNSmall(input_dim=args.embedding_dim, num_objects=args.num_objects, hidden_dim=args.hidden_dim // 16, output_size=input_shape).to(device)
-
-    if os.path.exists(decoder_file):
-        decoder.load_state_dict(torch.load(decoder_file))
-        decoder.eval()
-    else:
-        print(f"Warning: {decoder_file} not found. Reconstruction loss will be skipped.")
-        decoder = None
-
+# topk = [1, 5, 10]
 topk = [1]
 hits_at = defaultdict(int)
 num_samples = 0
 rr_sum = 0
-bce_loss_sum = 0.0
 
 pred_states = []
 next_states = []
 
-print(f'Running eval on {args_eval.num_steps} steps...')
-
 with torch.no_grad():
+
     for batch_idx, data_batch in enumerate(eval_loader):
-        data_batch = [[t.to(device) for t in tensor] for tensor in data_batch]
+        data_batch = [[t.to(
+            device) for t in tensor] for tensor in data_batch]
         observations, actions = data_batch
 
         if observations[0].size(0) != args.batch_size:
@@ -113,41 +100,59 @@ with torch.no_grad():
             pred_trans = model.transition_model(pred_state, actions[i])
             pred_state = pred_state + pred_trans
 
-        if decoder is not None:
-            pred_rec = torch.sigmoid(decoder(pred_state))
-            bce_loss_sum += F.binary_cross_entropy(pred_rec, next_obs, reduction='sum').item()
-
         pred_states.append(pred_state.cpu())
         next_states.append(next_state.cpu())
 
     pred_state_cat = torch.cat(pred_states, dim=0)
     next_state_cat = torch.cat(next_states, dim=0)
+
     full_size = pred_state_cat.size(0)
 
+    # Flatten object/feature dimensions
     next_state_flat = next_state_cat.view(full_size, -1)
     pred_state_flat = pred_state_cat.view(full_size, -1)
 
-    dist_matrix = utils.pairwise_distance_matrix(pred_state_flat, next_state_flat)
-    dist_np = dist_matrix.numpy()
-    indices = np.stack([np.lexsort((np.arange(len(row)), row)) for row in dist_np], axis=0)
+    dist_matrix = utils.pairwise_distance_matrix(
+        next_state_flat, pred_state_flat)
+    dist_matrix_diag = torch.diag(dist_matrix).unsqueeze(-1)
+    dist_matrix_augmented = torch.cat(
+        [dist_matrix_diag, dist_matrix], dim=1)
+
+    # Workaround to get a stable sort in numpy.
+    dist_np = dist_matrix_augmented.numpy()
+    indices = []
+    for row in dist_np:
+        keys = (np.arange(len(row)), row)
+        indices.append(np.lexsort(keys))
+    indices = np.stack(indices, axis=0)
     indices = torch.from_numpy(indices).long()
 
-    labels = torch.arange(full_size).unsqueeze(-1)
+    print('Processed {} batches of size {}'.format(
+        batch_idx + 1, args.batch_size))
 
-    print('Processed {} batches of size {}'.format(batch_idx + 1, args.batch_size))
+    labels = torch.zeros(
+        indices.size(0), device=indices.device,
+        dtype=torch.int64).unsqueeze(-1)
+
     num_samples += full_size
+    print('Size of current topk evaluation batch: {}'.format(
+        full_size))
 
     for k in topk:
-        hits_at[k] += (indices[:, :k] == labels).sum().item()
+        match = indices[:, :k] == labels
+        num_matches = match.sum()
+        hits_at[k] += num_matches.item()
 
-    _, ranks = (indices == labels).max(1)
-    rr_sum += torch.reciprocal(ranks.double() + 1).sum().item()
+    match = indices == labels
+    _, ranks = match.max(1)
+
+    reciprocal_ranks = torch.reciprocal(ranks.double() + 1)
+    rr_sum += reciprocal_ranks.sum()
+
+    pred_states = []
+    next_states = []
 
 for k in topk:
     print('Hits @ {}: {}'.format(k, hits_at[k] / float(num_samples)))
 
 print('MRR: {}'.format(rr_sum / float(num_samples)))
-
-if decoder is not None:
-    avg_pixel_bce = bce_loss_sum / (num_samples * input_shape.numel())
-    print('Pixel BCE Loss: {:.6f}'.format(avg_pixel_bce))
